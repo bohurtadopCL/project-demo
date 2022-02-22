2a61641b1de9ad7c659bc20bb803dbfc4900879a
Brayan Hurtado <brayan.hurtado@condorlabs.io>
2022-02-22 18:45:38 -0500

    fixing bug in redis + docker

--- src/redis/redis.js
+++ src/redis/redis.js
@@ -2,7 +2,9 @@ const redis = require('redis');
 const logger = require('@condor-labs/logger');
 const { settings } = require('../../config/constants');
 
-const client = redis.createClient(settings.port);
+const client = redis.createClient({
+  url: 'redis://'.concat(settings.host, ':', settings.port),
+});
 
 client.on('error', (err) => {
   logger.log(err);
